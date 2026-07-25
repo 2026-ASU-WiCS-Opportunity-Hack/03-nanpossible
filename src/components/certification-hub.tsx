ea61f4923a57390cbb9770164c9b5bfb90be876e
Ehitnesh Tadese <ehitnesh.k.tadese@eagles.oc.edu>
2026-07-25 09:28:42 -0500

    fixed the contact us colour contrast issue in certification page on the landing page

--- src/components/certification-hub.tsx
+++ src/components/certification-hub.tsx
@@ -285,12 +285,10 @@ export function CertificationHubSections() {
           Contact us to learn more about WIAL certification programs and find a
           certification path that is right for you.
         </p>
-        <Link
-          href="/contact"
-          className="mt-4 inline-block rounded-md bg-teal-deep px-6 py-2.5 text-sm font-semibold text-white transition hover:bg-teal-deep/90"
-        >
-          Contact Us
-        </Link>
+      <br/>
+      <p>
+        <a className="button-link primary" href="/contact">Contact Us</a>
+        </p>
       </div>
     </div>
   );
